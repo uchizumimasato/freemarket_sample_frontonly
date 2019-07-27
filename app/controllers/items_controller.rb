2b79fae3a9a9d81df0b29dbac332cd1ad39de7ca
uchizumimasato <uchizumimasato@users.noreply.github.com>
2019-07-27 12:42:08 +0900

    create item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :login_required, only: [:new, :create]
+
   def index
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    item = Item.new(items_params)
+
+    if item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def items_params
+    params.require(:item).permit(:name, :description, :item_status, :delivery_fee, :delivery_method, :delivery_date, :prefecture, :price, :size, :transaction_status, :category_id, :brand_id, :seller_id, :buyer_id, {images: []})
   end
 end
