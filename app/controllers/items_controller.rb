96c48aa458c719441c3a62f5a1bfb5affc8f4c59
utcizumi masato <24700269+uchizumimasato@users.noreply.github.com>
2019-08-21 18:30:30 +0900

    change prefecture_id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,11 +6,16 @@ class ItemsController < ApplicationController
     @brand_items = Item.where(brand_id: 1).limit(4)
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def new
     @item = Item.new
   end
 
   def create
+    binding.pry
     item = Item.new(items_params)
     if item.save
       redirect_to root_path
@@ -22,6 +27,6 @@ class ItemsController < ApplicationController
   private
 
   def items_params
-    params.require(:item).permit(:name, :description, :item_status, :delivery_fee, :delivery_method, :delivery_date, :prefecture, :price, :size, :transaction_status, :category_id, :brand_id, :seller_id, :buyer_id, {images: []})
+    params.require(:item).permit(:name, :description, :delivery_fee, :delivery_method, :delivery_date, :prefecture_id, :price, :item_status, :size, :transaction_status, :category_id, :brand_id, :seller_id, :buyer_id, {images: []})
   end
 end
