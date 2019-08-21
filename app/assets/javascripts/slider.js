83b4e87d52f16af567075bfe67f0d460c55cc3a8
utcizumi masato <24700269+uchizumimasato@users.noreply.github.com>
2019-08-21 12:08:24 +0900

    delete commentout

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,22 +1,10 @@
 $(function() {
   $("#slickLists--0").slick({
-    //centerMode: true,
     centerPadding: '100px',
-    //infinite: true,
-    //slidesToShow: 3,
-    //slidesToScroll: 1,
-    //autoplay: true,
-    //autoplaySpeed: 2000,
   });
 });
 $(function() {
   $("#slickLists--1").slick({
-    centerMode: true,
     centerPadding: '100px',
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
   });
 });
