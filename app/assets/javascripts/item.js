5579d766429406a93edf6762ca5809f7b8894bd4
utcizumi masato <24700269+uchizumimasato@users.noreply.github.com>
2019-08-26 13:21:45 +0900

    edit item function

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -4,12 +4,14 @@ $(function(){
   function image(reader, file, img_count) {
     reader.onload = (function(file) {
     return function(e) {
-      $('.productForm__field--top').append(`<img id=img-${img_count}>`)
-      $(`#img-${img_count}`).attr("src", e.target.result)
-      $(`#img-${img_count}`).css({"margin-left": "12px",
-                                  "height": "224px",
-                                  "width": "21%",
-                                  "border": "1px dashed #ccc"});
+      var html_image = `<figure class='form_item'>
+                         <img class='item_image js_image' src=${e.target.result}>
+                         <div class='item_btn'>
+                           <div class='item_edit_btn'>編集</div>
+                           <div class='item_delete_btn' data-img-index=${img_count}>削除</div>
+                         </div>
+                        </figure>`
+      $('.productForm__field--top').append(html_image)
     }
   })(file)
   reader.readAsDataURL(file)
@@ -18,20 +20,53 @@ $(function(){
   $('.new_item').on('change', 'input[type="file"]', function(e) {
     var file = e.target.files[0]
     var reader = new FileReader()
-    var img_count = $('.productForm__field--top img').length + 1
-    var persent = 100 - ('25' * img_count)
+    var js_img_count = $('.productForm__field--top js_image').length
+    var img_count = $('.productForm__field--top .form_item').length
+    var persent = 100 - ('25' * (img_count + 1))
+    if (img_count == 4) {
+      $('.uploadInfo').remove()
+    }
+
+    var last_label = $($('.formUploader label')[img_count - 1])
+    var label_index = $(last_label).data('label-index')
+    var label = `<label data-label-index=${label_index + 1}>
+                  <input class='avatar' multiple='multiple' type='file' name='item[images][]'>
+                  <div class='uploadInfo'>
+                    <p>ドラッグアンドドロップ</p>
+                    <span>またはクリックしてアップロード</span>
+                  </div>
+                </label>`
+    image(reader, file, $(label).data('label-index'))
     $('.formUploader').css('width', persent + '%')
+    $($('.formUploader label')[img_count - 1]).css('display','none')
+    $('.formUploader').append(label)
+  })
 
-    image(reader, file, img_count)
+  // プレビュー削除の関数
+  $('.productForm__field--top').on('click', '.item_delete_btn', function() {
+    var img_index = $(this).data('img-index')
 
-    $(`#${img_count}`).hide()
-    var label = `<label id=${img_count + 1}>
-                   <input class="avatar" multiple="multiple" type="file" name='item[images][]'>
-                   <div class='uploadInfo'>
-                     <p>ドラッグアンドドロップ</p>
-                     <span>またはクリックしてアップロード</span>
-                   </div>
-                 </label>`
+    if ($(this).attr('id') != undefined) {
+      delete_id = $(this).attr('id')
+      $('.productForm__field--top').append(`<input type="hidden" name='[delete_ids][]' value=${delete_id}>`)
+    }
+    $(this).parent().parent().remove()
+var img_count = $('.productForm__field--top .form_item').length
+    $.each($('.formUploader label'), function(i, label) {
+      if ($(label).data('label-index') == img_index) {
+        $(label).remove()
+      }
+    })
+    $('.formUploader').css('width', 100 - ($('.productForm__field--top figure').length * 25) + '%')
+    var last_label = $($('.formUploader label')[img_count - 1])
+    var label_index = $(last_label).data('label-index')
+    var label = `<label data-label-index=${label_index + 1}>
+                  <input class='avatar' multiple='multiple' type='file' name='item[images][]'>
+                  <div class='uploadInfo'>
+                    <p>ドラッグアンドドロップ</p>
+                    <span>またはクリックしてアップロード</span>
+                  </div>
+                </label>`
     $('.formUploader').append(label)
   })
 
@@ -44,8 +79,8 @@ $(function(){
   // 子カテゴリーの選択
   $('#inputField__select--category').on('change', function() {
     var child_categories = {2: 'トップス', 3: 'ジャケット/アウター'}
-    var select = `<select class="inputField inputField__select" id="inputField__select--category--child" name=item[category_id] required='required'>
-                    <option>---</option>
+    var select = `<select class="inputField inputField__select" id="inputField__select--category--child" name='category_id' required='required'>
+                    <option value>---</option>
                   </select>`
     var select_category_child = $('#inputField__select--category--child')
     $(select_category_child).remove()
@@ -66,8 +101,7 @@ $(function(){
                   <option>---</option>
                 </select>`
     $(select_category_grandchild).remove()
-
-    if ($('#inputField__select--category--child').val() != '---') {
+    if ($('#inputField__select--category--child').val() != '') {
       $('#inputField__select--category--child').after(select)
       $.each(grandchild_categories, function(id, name) {
         if (category_child_text == 'トップス' && id <= 6) {
@@ -81,7 +115,7 @@ $(function(){
 
   $('.productBtn').on('submit', function() {})
 
-  // 配送の関数
+  // 配送方法の関数
   $('#inputField__select--fee').on('change', function() {
     $('.delivery_method').remove()
     var val = $('#inputField__select--fee').val() == "" ? 3 : $('#inputField__select--fee').val()
@@ -90,31 +124,31 @@ $(function(){
                      <label>配送の方法</label>
                      <span class='formRequired'>必須</span>
                      <br>
-                     <select class='inputField inputField__select' name='item[delivery_method]'>
+                     <select class='inputField inputField__select' name='item[delivery_method_id]'>
                        <option>---</option>
-                       <option value=0>未定</option>
-                       <option value=1>らくらくメルカリ便</option>
-                       <option value=2>ゆうメール</option>
-                       <option value=3>レターパック</option>
-                       <option value=4>普通郵便</option>
-                       <option value=5>クロネコヤマト</option>
-                       <option value=6>ゆうパック</option>
-                       <option value=7>クリックポスト</option>
-                       <option value=8>ゆうパケット</option>
+                       <option value=1>未定</option>
+                       <option value=2>クロネコヤマト</option>
+                       <option value=3>ゆうパック</option>
+                       <option value=4>ゆうメール</option>
+                       <option value=5>らくらくメルカリ便</option>
+                       <option value=6>レターパック</option>
+                       <option value=7>普通郵便</option>
+                       <option value=8>クリックポスト</option>
+                       <option value=9>ゆうパケット</option>
                      </select>
                    </div>`
       $('.inputDelivery_fee').after(select)
     } else if (val == "着払い（購入者負担）") {
-      var select = `<div class='productForm__field delivery_method' name='item[delivery_method'>
+      var select = `<div class='productForm__field delivery_method' name='item[delivery_method_id'>
                      <label>配送の方法</label>
-                     <span class='formRequired'>1</span>
+                     <span class='formRequired'>必須</span>
                      <br>
                      <select class='inputField inputField__select'>
                        <option>---</option>
-                       <option value=0>未定</option>
-                       <option value=5>クロネコヤマト</option>
-                       <option value=6>ゆうパック</option>
-                       <option value=2>ゆうメール</option>
+                       <option value=1>未定</option>
+                       <option value=2>クロネコヤマト</option>
+                       <option value=3>ゆうパック</option>
+                       <option value=4>ゆうメール</option>
                      </select>
                    </div>`
       $('.inputDelivery_fee').after(select)
