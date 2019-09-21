45bb7b2996c6e8954a3273898c97eddcfbc44e65
uchizumimasato <uchizumimasato@users.noreply.github.com>
2019-09-22 03:08:54 +0900

    add sns routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: "items#index"
   resources :sessions, only: [:new, :create, :destroy]
-  resources :registrations, only: :new do
+  resources :registrations, only: [:new, :create] do
     collection do
-      get 'first'
+      get 'first', 'sns'
       post 'second', 'third', 'forth', 'fifth'
     end
   end
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
     end
   end
   resources :cards, only: [:new, :create]
+  get "auth/:provider/callback" => 'registrations#sns'
 end
